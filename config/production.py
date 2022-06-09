be55609c73c8f722a8cd9d29be14836022b6a873
Keisha Louise Vivien Berondo <berondovivien@gmail.com>
2022-06-09 19:29:08 +0800

    Added configurations to production

--- config/production.py
+++ config/production.py
@@ -25,4 +25,14 @@ TELESIGN_CUSTOMER_ID = env("TELESIGN_CUSTOMER_ID")
 # TWILIO_MSGG_SERVICE_SID = env("TWILIO_MSGG_SERVICE_SID", default="")
 # TWILIO_CONVERSATIONS_SERVICE_SID = env("TWILIO_CONVERSATIONS_SERVICE_SID", default="")
 
-
+# CSRF
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-include-subdomains
+# TODO: set this to 60 seconds first and then to 518400 once you prove the former works
+SECURE_HSTS_SECONDS = 60
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_CONTENT_TYPE_NOSNIFF =True
